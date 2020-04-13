7d3a8c6ee15b7bc665fb1f335017c4f22088f723
phimwadi <phimwadi0033@gmail.com>
2020-04-13 17:04:38 +0700

    commit2

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDUDP9zi9u-amV0Tu8TZfIOvGZyV2Y14L4",
+    authDomain: "login-22fab.firebaseapp.com",
+    databaseURL: "https://login-22fab.firebaseio.com",
+    projectId: "login-22fab",
+    storageBucket: "login-22fab.appspot.com",
+    messagingSenderId: "421835624325",
+    appId: "1:421835624325:web:b60daaf9c81b8f3298feec",
+    measurementId: "G-F03SNRVGSL"
+  }
+  
+  export default config
